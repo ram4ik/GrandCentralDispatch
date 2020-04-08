2bdbee933dc6d0265998fe35554a4a11952490fd
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-04-08 11:31:25 +0300

    init

--- GrandCentralDispatch/ContentView.swift
+++ GrandCentralDispatch/ContentView.swift
@@ -10,7 +10,78 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Button(action: {
+                self.doItInGlobalAsync()
+            }) {
+                Text("GCD async")
+            }
+            Button(action: {
+                self.doItInGlobalSync()
+            }) {
+                Text("GCD sync")
+            }
+        }.padding()
+    }
+    
+    func doItInGlobalAsync() {
+        // High
+        DispatchQueue.global(qos: .userInteractive).async {
+            print("1 - High: .userInteractive")
+        }
+        
+        // Default
+        DispatchQueue.global(qos: .userInitiated).async {
+            print("2 - Default: .userInitiated")
+        }
+        
+        DispatchQueue.global(qos: .default).async {
+            print(".default")
+        }
+        
+        // Low
+        DispatchQueue.global(qos: .utility).async {
+            print("3 - Low: .utility")
+        }
+        
+        // Background
+        DispatchQueue.global(qos: .background).async {
+            print("4 - In Background: .background")
+        }
+        
+        DispatchQueue.global(qos: .unspecified).async {
+            print(".unspecified")
+        }
+    }
+    
+    func doItInGlobalSync() {
+        // High
+        DispatchQueue.global(qos: .userInteractive).sync {
+            print("1 - High: .userInteractive")
+        }
+        
+        // Default
+        DispatchQueue.global(qos: .userInitiated).sync {
+            print("2 - Default: .userInitiated")
+        }
+        
+        DispatchQueue.global(qos: .default).sync {
+            print(".default")
+        }
+        
+        // Low
+        DispatchQueue.global(qos: .utility).sync {
+            print("3 - Low: .utility")
+        }
+        
+        // Background
+        DispatchQueue.global(qos: .background).sync {
+            print("4 - In Background: .background")
+        }
+        
+        DispatchQueue.global(qos: .unspecified).sync {
+            print(".unspecified")
+        }
     }
 }
 
